a2c6c380213b4e5a949d7921c96ec4e43ac031d6
RhinosF1 <rhinosf1@gmail.com>
2020-10-29 15:36:09 +0000

    add test

---
+++ tests/test_access_from_json.py
@@ -0,0 +1,18 @@
+from sopel_channelmgnt.channelmgnt import get_access
+
+DATA = (open('test_config.json', 'r')).read()
+
+
+def test_simple():
+    assert get_access("##test-channel", DATA, "Paladox") == True
+
+
+def test_with_space():
+    assert get_access("##test-channel", DATA, "Universal_Omega") == True
+
+
+def test_with_number():
+    assert get_access("##test-channel", DATA, "RhinosF1") == True
+
+def test_false():
+    assert get_access("##test-channel", DATA, "JohnLewis") == False
