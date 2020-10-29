8089752a0d3bf9682ad776b83fabf45d71e5251a
RhinosF1 <rhinosf1@gmail.com>
2020-10-29 17:05:57 +0000

    Fix pep8

--- tests/test_access_from_json.py
+++ tests/test_access_from_json.py
@@ -5,15 +5,15 @@ DATA = jp.createdict('tests/test_config.json')
 
 
 def test_simple():
-    assert get_access("##test-channel", DATA, "Paladox") == True
+    assert get_access("##test-channel", DATA, "Paladox")
 
 
 def test_with_space():
-    assert get_access("##test-channel", DATA, "Universal_Omega") == True
+    assert get_access("##test-channel", DATA, "Universal_Omega")
 
 
 def test_with_number():
-    assert get_access("##test-channel", DATA, "RhinosF1") == True
+    assert get_access("##test-channel", DATA, "RhinosF1")
 
 def test_false():
-    assert get_access("##test-channel", DATA, "JohnLewis") == False
+    assert not get_access("##test-channel", DATA, "JohnLewis")
