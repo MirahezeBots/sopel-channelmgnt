f39e194c85f8f3bb1fbe54a8409669cf80d3027e
RhinosF1 <rhinosf1@gmail.com>
2020-10-29 18:48:10 +0000

    Update test_access_from_json.py

--- tests/test_access_from_json.py
+++ tests/test_access_from_json.py
@@ -2,19 +2,20 @@ from sopel_channelmgnt.channelmgnt import get_access
 from MirahezeBots_jsonparser import jsonparser as jp
 
 DATA = jp.createdict('tests/test_config.json')
+TEST_DATA_CHANNEL
 
 
 def test_simple():
-    assert get_access("##test-channel", DATA, "Paladox")
+    assert get_access(TEST_DATA_CHANNEL, DATA, "Paladox")
 
 
 def test_with_space():
-    assert get_access("##test-channel", DATA, "Universal_Omega")
+    assert get_access(TEST_DATA_CHANNEL, DATA, "Universal_Omega")
 
 
 def test_with_number():
-    assert get_access("##test-channel", DATA, "RhinosF1")
+    assert get_access(TEST_DATA_CHANNEL, DATA, "RhinosF1")
 
 
 def test_false():
-    assert not get_access("##test-channel", DATA, "JohnLewis")
+    assert not get_access(TEST_DATA_CHANNEL, DATA, "JohnLewis")
