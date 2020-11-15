262c51f4d5357ef416e7083d0549a1f040297413
RhinosF1 <rhinosf1@gmail.com>
2020-11-15 22:09:08 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='sopel_plugins.channelmgnt',
-    version='1.1.0',
+    version='1.0.3',
     description='Channelmgnt plugin for Sopel',
     long_description=readme,
     long_description_content_type='text/markdown',  # This is important!
